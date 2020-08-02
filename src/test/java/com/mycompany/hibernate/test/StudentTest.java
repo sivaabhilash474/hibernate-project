ca0f52065906641d6d6d47dadcabee5765bbc1a4
siva <sivaabhilash474@gmail.com>
2020-08-02 18:12:59 -0400

    pom.xml, studentTest.java

--- src/test/java/com/mycompany/hibernate/test/StudentTest.java
+++ src/test/java/com/mycompany/hibernate/test/StudentTest.java
@@ -1,11 +1,9 @@
 package com.mycompany.hibernate.test;
 
 import java.io.File;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
 import com.mycompany.hibernate.beans.Student;
 
 public class StudentTest {
